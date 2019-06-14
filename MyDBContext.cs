a4cc6916e6e2f044576ffaefa9f05abb3103ee55
Matheus Maroli <matheus.maroli66@gmail.com>
2019-06-14 16:39:47 -0300

    Add files via upload

---
+++ MyDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.SQLite.Teste.Model
+{
+
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+            : base(new SQLiteConnection()
+            {
+                ConnectionString =
+                    new SQLiteConnectionStringBuilder() { DataSource = "C:\\WpfSQLiteSQLite\\dbteste.db" }
+                    .ConnectionString
+            }, true)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.SQLite.Teste.Model
+{
+    [Table("UserInfo")]
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
